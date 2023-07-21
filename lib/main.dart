fc5eaea005fd283404599228281cf17e9756bded
SS08090 <techicalsolutions68@gmail.com>
2023-07-21 22:11:04 +0530

    Progress in ui

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,42 @@ import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'package:flutter/services.dart'; // For permission handling
 import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
 }
+
+List<String> feedItems = [];
+List<String> feedId = [];
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final IconData icon;
+
+  const CustomFloatingActionButton({
+    required this.onPressed,
+    required this.backgroundColor,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      shape: CircleBorder(),
+      elevation: 4.0,
+      child: FloatingActionButton(
+        onPressed: onPressed,
+        backgroundColor: backgroundColor,
+        child: Icon(icon),
+      ),
+    );
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,11 +47,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Blackhole',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Blackhole Downloader'),
     );
   }
 }
@@ -34,7 +66,68 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    var driveurl;
+  String serverurl = 'original-google.onrender.com';
+  Color fabColor = Colors.blue;
+  Map<String, bool> buttonStatusMap = {};
+  Timer? periodicTimer; // Timer instance
+
+    void getfiles() async {
+    try {
+      var uri = Uri.https(serverurl, '/getfiles');
+      http.Response response = await http.get(uri);
+      if (response.statusCode == 200) {
+        debugPrint(response.body.toString());
+        feedId.clear();
+        feedItems.clear();
+        List<dynamic> jsonList = json.decode(response.body.toString());
+        for (var item in jsonList) {
+          if (item['mimeType'] != 'application/vnd.google-apps.folder') {
+            String name = item['name'];
+            String id = item['id'];
+            feedItems.add('$name'); /*$name - $id*/
+            feedId.add('$id');
+          }
+        }
+        print(feedItems);
+        setState(() {
+          driveurl = response.body.toString();
+          buttonStatusMap =
+              Map.fromIterable(feedId, key: (id) => id, value: (_) => true);
+        });
+      } else {
+        print('Request failed with status: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  void online_test_url() {
+    void fetchStatus() async {
+      try {
+        final response = await http.get(Uri.parse('https://' + serverurl));
+        if (response.statusCode == 200) {
+          setState(() {
+            fabColor = Colors.green;
+          });
+        } else {
+          setState(() {
+            fabColor = Colors.red;
+          });
+        }
+      } catch (e) {
+        setState(() {
+          fabColor = Colors.red;
+        });
+      }
+    }
+
+    periodicTimer = Timer.periodic(Duration(seconds: 30), (_) {
+      fetchStatus();
+      print('periodicTimer');
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -83,7 +176,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -93,21 +186,60 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: ListView.builder(
+                itemCount: feedItems.length,
+                itemBuilder: (context, index) {
+                  String id = feedId[index];
+                  bool isButtonEnabled = buttonStatusMap[id] ?? true;
+                  return Card(
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            feedItems[index],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed:  _incrementCounter,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    isButtonEnabled ? Colors.blue : Colors.grey,
+                              ),
+                              child: Text('Download'), /*$index*/
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+                  floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomFloatingActionButton(
+            onPressed: online_test_url,
+            backgroundColor: fabColor,
+            icon: Icons.power,
+          ),
+          SizedBox(height: 12), // Adjust the spacing between the FABs
+          CustomFloatingActionButton(
+            onPressed: getfiles,
+            backgroundColor: Colors.orange, // Set the desired background color
+            icon: Icons.file_copy_sharp, // Set the desired icon
+          ),
+        ],
       ),
     );
   }
 }
+
